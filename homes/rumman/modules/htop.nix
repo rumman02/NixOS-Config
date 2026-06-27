f7bf83ba94b3ff6b81f245ebaa90ddb909750344
rumman02 <126387776+rumman02@users.noreply.github.com>
2026-06-27 23:47:20 +0600

    feat(nix/modules): add htop module

---
+++ homes/rumman/modules/htop.nix
@@ -0,0 +1,12 @@
+{
+
+	# ===========================================================================
+	# HTOP (INTERACTIVE PROCESS VIEWER)
+	# ===========================================================================
+	programs.htop = {
+		enable = true;
+		settings = {
+			delay = 5; # Delay between updates in tenths of seconds
+		};
+	};
+}
