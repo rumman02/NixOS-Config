99f3cb5cac321d63228506d7cdfd997d6dc66b4e
rumman02 <126387776+rumman02@users.noreply.github.com>
2026-06-27 22:38:32 +0600

    feat(hosts/rumman_dp): add services (sddm, dbus, udisks2), kernel modules, hardware i2c

--- hosts/rumman_dp/configuration.nix
+++ hosts/rumman_dp/configuration.nix
@@ -8,7 +8,11 @@
     enable = true;
     efiSupport = true;
     devices = [ "nodev" ];
+    configurationLimit = 20;
+    useOSProber = false;
   };
+
+  boot.kernelModules = [ "uinput" "i2c-dev" ];
 
   networking.hostName = hostname;
   networking.networkmanager.enable = true;
@@ -18,8 +22,35 @@
     isNormalUser = true;
     home = homedir;
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "i2c"
+      "uinput"
+      "input"
+      "audio"
+    ];
+    packages = with pkgs; [
+      home-manager
+      firefox
+      ghostty
+      ddcutil
+    ];
   };
+
+  services = {
+    dbus.enable = true;
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      package = pkgs.kdePackages.sddm;
+    };
+    udisks2.enable = true;
+    timesyncd.enable = true;
+  };
+
+  powerManagement.enable = true;
+  hardware.i2c.enable = true;
 
   system.stateVersion = stateversion;
 }
