8146939758b87701af9b67234c6e3ae4d79c588a
rumman02 <126387776+rumman02@users.noreply.github.com>
2026-06-27 22:39:18 +0600

    feat(hosts/rumman_dp): add fonts, udev rules, hyprland, system packages, nix settings

--- hosts/rumman_dp/configuration.nix
+++ hosts/rumman_dp/configuration.nix
@@ -44,6 +44,7 @@
       enable = true;
       wayland.enable = true;
       package = pkgs.kdePackages.sddm;
+      theme = "catppuccin-mocha-mauve";
     };
     udisks2.enable = true;
     timesyncd.enable = true;
@@ -52,5 +53,60 @@
   powerManagement.enable = true;
   hardware.i2c.enable = true;
 
-  system.stateVersion = stateversion;
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{power/wakeup}=="disabled", ATTR{power/wakeup}="enabled"
+    SUBSYSTEM=="input", KERNEL=="event*", ATTRS{power/wakeup}=="disabled", ATTR{power/wakeup}="enabled"
+    KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
+    KERNEL=="uinput", GROUP="input", TAG+="uaccess"
+    KERNEL=="event*", NAME="input/%k", MODE="0664", GROUP="input"
+  '';
+
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      noto-fonts-color-emoji
+    ];
+  };
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = false;
+    portalPackage = pkgs.xdg-desktop-portal-hyprland;
+  };
+
+  programs.zsh.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    fzf git neovim tree yazi unzip xdg-user-dirs
+  ];
+
+  time.timeZone = "Asia/Dhaka";
+
+  system = {
+    stateVersion = stateversion;
+    autoUpgrade = {
+      enable = true;
+      dates = "weekly";
+    };
+  };
+
+  nix = {
+    settings = {
+      stalled-download-timeout = 300;
+      substituters = [ "https://cache.nixos.org" "https://cache.nixos.org/" ];
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
 }
