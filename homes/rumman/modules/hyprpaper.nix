6a834317944d39cc005dab1e7a2d11d3af3f1dbc
rumman02 <126387776+rumman02@users.noreply.github.com>
2026-06-27 23:47:20 +0600

    feat(nix/modules): add hyprpaper module

---
+++ homes/rumman/modules/hyprpaper.nix
@@ -0,0 +1,15 @@
+{
+
+	# ===========================================================================
+	# HYPRPAPER (WALLPAPER DAEMON FOR HYPRLAND)
+	# ===========================================================================
+	services.hyprpaper = {
+		enable = true;
+		settings = {
+			ipc = "on";
+			splash = false;
+			splash_offset = 2.0;
+		};
+	};
+}
+
