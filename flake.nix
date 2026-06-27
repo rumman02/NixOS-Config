85880223ce39b2fee0c91be7031eb24e3b3b1883
rumman02 <126387776+rumman02@users.noreply.github.com>
2026-06-27 22:24:03 +0600

    feat: add home-manager and nixpkgs-25.05 stable

--- flake.nix
+++ flake.nix
@@ -3,8 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-alt.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-alt, home-manager, ... }@inputs: {
   };
 }
