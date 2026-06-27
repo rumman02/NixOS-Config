291140a96fce39799de80fdc00e71338a4722be3
rumman02 <126387776+rumman02@users.noreply.github.com>
2026-06-27 22:25:07 +0600

    feat: add mkSystem/mkHome helpers with specialArgs

--- flake.nix
+++ flake.nix
@@ -20,6 +20,92 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-alt, home-manager, ... }@inputs: {
-  };
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-alt,
+    home-manager,
+    ...
+  }@inputs:
+    let
+      defaultSys = "x86_64-linux";
+      defaultStateV = "25.05";
+      # function
+      mkPkgs =
+        pkgsSource: system:
+        import pkgsSource {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            allowUnsupportedSystem = true;
+          };
+        };
+      pkgsPri = mkPkgs nixpkgs;
+      pkgsAlt = mkPkgs nixpkgs-alt;
+
+      # system
+      mkSystem = {
+        system ? defaultSys,
+        specialArgs ? { },
+        modules ? [ ],
+        homedir,
+        configdir,
+        hostname,
+        username,
+        stateversion ? defaultStateV,
+      }:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          pkgs = pkgsPri system;
+          specialArgs = {
+            alt = pkgsAlt system;
+            inherit
+            homedir
+            hostname
+            username
+            configdir
+            stateversion
+            inputs
+            ;
+          }
+            // specialArgs;
+          modules = [
+            ./hosts/${hostname}/configuration.nix
+          ]
+            ++ modules;
+        };
+
+      # home
+      mkHome = {
+        system ? defaultSys,
+        extraSpecialArgs ? { },
+        modules ? [ ],
+        homedir,
+        configdir,
+        hostname,
+        username,
+        stateversion ? defaultStateV,
+      }:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgsPri system;
+          extraSpecialArgs = {
+            alt = pkgsAlt system;
+            inherit
+            system
+            homedir
+            hostname
+            username
+            configdir
+            stateversion
+            inputs
+            ;
+          }
+            // extraSpecialArgs;
+          modules = [
+            ./homes/${username}/home.nix
+          ]
+            ++ modules;
+        };
+    in {
+    };
 }
