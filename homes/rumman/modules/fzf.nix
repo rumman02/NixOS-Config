14fb794f33216e23c0d40af010b8548e08dbc71d
rumman02 <126387776+rumman02@users.noreply.github.com>
2026-06-27 23:47:16 +0600

    feat(nix/modules): add fzf module

---
+++ homes/rumman/modules/fzf.nix
@@ -0,0 +1,11 @@
+{
+
+	# ===========================================================================
+	# FZF (COMMAND-LINE FUZZY FINDER)
+	# ===========================================================================
+	programs.fzf = {
+		enable = true;
+		# enableZshIntegration = true;
+		# enableBashIntegration = true;
+	};
+}
